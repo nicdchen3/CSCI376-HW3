5fd049cc3015812758900488c970900f49ac709d
Nicholas <ndc2@williams.edu>
2025-03-10 11:47:31 -0400

    In-class demo

---
+++ nicegui-demo.py
@@ -0,0 +1,8 @@
+from nicegui import ui
+
+with ui.row().classes("mx-auto"):
+    with ui.card():
+        input_field = ui.input(
+            on_change = lambda e: result.set_text(e.value.lower()))
+        result = ui.label()
+ui.run()
